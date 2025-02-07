42db0e0439af991ac42d44bff680d671460b264d
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-02-07 15:47:26 +0530

    complete

--- class14 ex-2/src/app.js
+++ class14 ex-2/src/app.js
@@ -8,25 +8,41 @@ const model = require('./models/user.model')
 
 app.use(express.json())
 
+app.set('view engine' , 'ejs')
+
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    
+    res.render('index')
+})
+
 app.post('/create', async (req,res)=>{
     const {username,email,imageurl} = req.body
 
     console.log(username,email,imageurl);
     
+    try{
+        const user  = await model.create({
+            username:username,
+            email:email,
+            imageurl:imageurl
+        })
+       res.redirect("/feed")
     
-    const user  = await model.create({
-        username:username,
-        email:email,
-        imageurl:imageurl
-    })
-   res.send(user)
+}
+catch (err){
+    console.log("error in creating user" , err);
+    res.send(err)
+    
+}
 })
 
 app.get('/feed',async(req,res)=>{
 
-    const users = await model.find()
-     console.log(users);
-     res.send(users)
+    const user = await model.find()
+
+   res.render("feed",{user:user})
      
  })
 
